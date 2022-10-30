e700326bd20909e72ebfc66a8559417d403cc6e6
Fabio Marsiaj <fabio.marsiaj@sysdata.com.br>
2022-10-30 19:01:56 -0300

    Select mostrando no path /

--- docker/nginx_with_node/node/index.js
+++ docker/nginx_with_node/node/index.js
@@ -11,14 +11,39 @@ const config = {
 const mysql = require('mysql')
 
 const insert = `INSERT INTO people(name) values('Fabio')`
+const sqlSelect = 'SELECT name FROM people'
 
 
 app.get('/', (req, res) => {
     const connection = mysql.createConnection(config)
-    connection.query(insert)
-    connection.end()
-    res.send('<h1>Full Cycle Rocks!</h1>')
+
+    let name = req.query.name;
+    if(typeof name !== 'undefined' && name){
+        insertSQL(name, connection)
+    }
+
+    var showPeoples = '<h1>Full Cycle Rocks!!!</h1><ul>'
+    connection.query(sqlSelect, function (err, result, fields) {
+        if (err) throw err;
+        console.log("Result:" + result);
+        result.forEach(element => {
+            showPeoples = showPeoples + '<li>' + element.name + '</li>' 
+        });
+        
+        showPeoples = showPeoples + '</ul>'
+        console.log("Peoples: " + showPeoples);
+        connection.end()
+        res.send(showPeoples)
+    });
 })
+
+
+function insertSQL(name, connection){
+    connection.query(sqlInsert,name, function (err, result) {
+        if (err) throw err;
+        console.log('Nome ' + name + ' registrado')
+    })
+}
 
 app.listen(port, () => {
     console.log('Rodando na porta ', + port)
